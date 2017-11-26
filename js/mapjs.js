c58e9fcf14fe01b72806d73cea2eb210cce7d322
Mohit Gupta <mohitg@adobe.com>
2017-11-26 23:12:23 +0530

    1. Logic to show markers for chosen schools 2. Created a class School that constructs from the model and adds custom information like markers. 3. Fixed an issue in the way lat and lng were stored. They need to be numbers and not strings

--- js/mapjs.js
+++ js/mapjs.js
@@ -4,49 +4,64 @@ var map;
 var schoolsModel = [
 	{
 		name: 'Lotus Valley International School, Sector 126',
-		lat: '28.5382382',
-		long: '77.34513330000004'
+		lat: 28.5382382,
+		long: 77.34513330000004
 	},
 	{
 		name: 'Step by Step School, Sector 132',
-		lat: '28.511652',
-		long: '77.378141'
+		lat: 28.511652,
+		long: 77.378141
 	},
 	{
 		name: 'Pathways School, Sector 100',
-		lat: '28.54226',
-		long: '77.367722'
+		lat: 28.54226,
+		long: 77.367722
 	},
 	{
 		name: 'The Shriram Millennium School',
-		lat: '28.499339',
-		long: '77.397373'
+		lat: 28.499339,
+		long: 77.397373
 	},
 	{
 		name: 'Gyanshree School, Sector 127',
-		lat: '28.536222',
-		long: '77.349374'
+		lat: 28.536222,
+		long: 77.349374
 	}
 ];
 
-function initmap() {
+function InitMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 28.5382382, lng: 77.34513330000004},
-    zoom: 16
+    	center: {lat: 28.5382382, lng: 77.34513330000004},
+    	zoom: 16
 	});
+}
+
+var School = function(value) {
+	var self = this;
+	this.name = value.name;
+	this.lat = value.lat;
+	this.long = value.long;
+
+	var myLatLng = {lat: this.lat, lng: this.long};
+	this.marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: this.name
+        });	
 }
 
 function SchoolViewModel() {
 	var self = this;
 	self.schoolList = ko.observableArray([]);
 
-	//Push all school names in an observable array
-	schoolsModel.forEach( function(school) {
-		self.schoolList.push(school);
-		});
-
 	//load map with default location
-	initmap();
+	InitMap();
+
+	//Push all school names in an observable array
+	schoolsModel.forEach( function(s) {
+		self.schoolList.push(new School(s));
+	});
+
 }	
 
 function initSchoolsApp() {
